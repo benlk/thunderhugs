5e118fa870c9fa868e6d66d049d4828d1f8733d4
Ben Keith <ben@benlk.com>
2017-01-02 12:15:02 -0500

    Add fetch.bash and improve readme

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ This bot aims to:
 
 Implementation ideas:
 
-- cron job: `fetch | parse | tweet` using an established tweet cli command, drawing from ENV vars
+- cron job: `fetch LAT LONG TWITTER_ACCOUNT | parse | tweet TWITTER_ACCOUNT` using an established tweet cli command, drawing from ENV vars
+- This is designed to not use environment variables, so that any one person can run multiple instances of the bot.
 
 # Setup
 
@@ -24,3 +25,34 @@ Implementation ideas:
 > <yano> and then grab the GPS coords for the zip code
 > <yano> and hard code that into it
 > <yano> and BAM
+
+### fetch
+
+This could probably just be a curl command
+
+See https://forecast-v3.weather.gov/documentation?redirect=legacy
+
+fetch.sh $1 $2 $3
+	$1 is the latitude
+	$2 is the longitude
+	$3 is the twitter account used for this bot; it's used in the useragent string
+
+### parse
+
+This needs to account for:
+
+- 200  success
+- any error message
+	- the message piped to 'tweet' should highlight the maintainer on Twitter
+		- maintainer twitter account should be an env var or a config variable, because no way nohow am I getting hundreds of bots @ing me.
+
+
+-----
+
+### Dependencies
+
+For Thunderhugs to run successfully, you need to have the following packages or programs installed:
+
+- getopts
+- curl
+- https://github.com/sferik/t
---
+++ fetch.bash
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# Initialize variables
+url='https://api-v1.weather.gov'
+#url='https://api.weather.gov'
+query='/points/'
+lat=''
+long=''
+verbose=''
+account='no twitter account set on command line'
+
+
+
+## check arguments
+function check_arguments {
+	echo 'tried to check arguments'
+	url=$1
+	query=$2
+	lat=$3
+	long=$4
+	verbose=$5
+	account=$6
+
+	# $lat should be a latitude
+	# $long should be a longitude
+	# if either of these are unset, show_help
+}
+
+function check_dependencies {
+	hash curl 2>/dev/null || {
+		echo "You need to install curl for thunderhugs to work."
+		echo "See https://github.com/benlk/thunderhugs/ for more information"
+		exit 1
+	}
+	hash getopts 2>/dev/null || {
+		echo "You need to install getopts for thunderhugs to work."
+		echo "See https://github.com/benlk/thunderhugs/ for more information"
+		exit 1
+	}
+}
+
+function show_help {
+	cat << EOF
+Thunderhugs fetch.bash version 0.0.1
+Usage:
+	fetch.bash [-h]
+	fetch.bash -a <account> -x <longitude> -y <latitude> [-v] [-u <URL>]
+
+This script queries the National Weather Service API for information based
+on the latitude and longitude given on the command line
+
+Arguments:
+	-h
+	-?
+		Display this help
+
+	-x
+		Set the longitude
+	
+	-y
+		Set the latitude
+
+	-v
+		Set verbosity
+	
+	-a
+		Set the twitter account you're using Thunderhugs for.
+		This is used in the useragent string when we query the NWS API.
+
+	-u
+		Set this to change the location of the NWS API.
+		By default, this is $url
+
+EOF
+}
+
+function fetch {
+	url=$1
+	query=$2
+	lat=$3
+	long=$4
+	verbose=$5
+	account=$6
+
+	exit 1;
+
+	curl \
+		-A "Thunderhugs version 0.0.1, "$account \
+		$verbose \
+		$url$query$long','$lat
+}
+
+#
+# The actual execution part of this script
+#
+
+check_dependencies
+
+# Parse options
+# see also http://aplawrence.com/Unix/getopts.html
+OPTIND=1
+while getopts "h?xyvau" opt; do
+	case "$opt" in
+		h|\?)
+			show_help
+			exit 0
+			;;
+		x)
+			# longitude
+			long=$OPTARG
+			echo "long: "$long
+			;;
+		y)
+			#latitude
+			lat=$OPTARG
+			echo "lat: "$lat
+			;;
+		v)
+			verbose=' --verbose'
+			echo "verbose: "$verbose
+			;;
+		a)
+			# twitter account
+			account=" twitter account "$OPTARG
+			echo "account: "$account
+			;;
+		u)
+			# NWS API Location
+			url=$OPTARG
+			echo "url: "$url
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+
+check_arguments $url $query $lat $long $verbose $account
+fetch $url $query $lat $long $verbose $account
