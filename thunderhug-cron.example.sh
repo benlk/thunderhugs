1339768b8d97bf976c1b5957290efd421e975ba3
Ben Keith <ben@benlk.com>
2017-01-02 15:54:52 -0500

    xargs t update

--- thunderhug-cron.example.sh
+++ thunderhug-cron.example.sh
@@ -3,4 +3,6 @@
 # every 30 minutes, from 8 a.m. to 10 p.m., every day
 # */30 8-21 * * * /full/path/to/thunderhug-cron.example.sh
 #
-./fetch.bash -x LAT -y LONG -a TWITTER_ACCOUNT | parse -m MAINTAINER_TWITTER | tweet TWITTER_ACCOUNT
+./fetch.bash -x LAT -y LONG -a TWITTER_ACCOUNT | \
+parse -m MAINTAINER_TWITTER | \
+xargs t update
